e2778d909180c054756e674a3a8518af61e3b532
delonge182 <elga_silaban@yahoo.com>
2019-01-25 02:38:02 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Performance evaluation of Terapixel rendering in Cloud (Super)computing',
+    author='efs',
+    license='MIT',
+)
